27c69d1807743f24f18c2438cff741a139f9ef08
Aquarion <nicholas@istic.net>
2013-06-11 22:36:27 +0100

    Add filebot scripts for organising media server

---
+++ mediaserver/sortitahht.bat
@@ -0,0 +1,2 @@
+;@echo off
+filebot -script fn:amc --output "C:/" --log-file amc.log --action move --conflict override -non-strict --def music=y subtitles=en artwork=y "ut_dir=%D" "ut_file=%F" "ut_kind=%K" "ut_title=%N" "ut_label=%L" "ut_state=%S" --def "seriesFormat=TV/{n}/{'S'+s}/{fn}" "animeFormat=Anime/{n}/{fn}" "movieFormat=Movies/{n} {y}/{fn}" "musicFormat=Music/{n}/{fn}" --def plex=localhost C:\Torrential
