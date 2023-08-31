94ad27b9c201aa66c42be54b0e9d5430e006278a
Nicholas Avenell <nicholas@istic.net>
2023-08-31 19:09:04 +0000

    Rework the upgrade wordpress script to be actually what it says it does

--- bashes/find-and-upgrade-wordpress.sh
+++ bashes/find-and-upgrade-wordpress.sh
@@ -14,12 +14,23 @@ echo "Looking in `realpath $searchdir`"
 find -L $searchdir -name wp-settings.php -not -regex ".*archive.*" 2> /dev/null | while read linein
 do
 	DIR=`dirname $linein`
+	echo -n `realpath $DIR`
 	VERSION=`wp core version --path="$DIR" --allow-root`
-	echo -n $VERSION at `realpath $DIR`
+	echo -n " is $VERSION"
 	if [[ $VERSION == $CURRENT ]];
 	then
-		echo " -- No Upgrade Required"
-	else
-		echo " -- Upgrade Required to $CURRENT" 
+	#	echo " -- No Upgrade Required"
+	#else
+		echo " -- Upgrade Required to $CURRENT"
+		pushd $DIR > /dev/null
+		OWNER=`stat -c '%U' wp-config.php`
+		sudo chown -R $OWNER:www-data .
+		echo -n "   ... [Core|"
+		sudo -u $OWNER wp core update | ts >> upgrade.log && \
+			echo -n "Plugins|" && sudo -u $OWNER wp plugin update --all | ts >> upgrade.log && \
+			echo -n "Themes|" && sudo -u $OWNER wp theme update --all | ts >> upgrade.log && \
+			echo -n "Plugin Lang|" && sudo -u $OWNER wp language plugin --all update | ts >> upgrade.log && \
+			echo "Theme Lang]" && sudo -u $OWNER wp language theme --all update | ts >> upgrade.log 
+		popd > /dev/null
 	fi
-done | sort | uniq
+done
