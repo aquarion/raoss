b06a8f6157c86624bddc9f88db42f87841c9cd2a
Nicholas@Cistern <nicholas@istic.net>
2023-10-05 20:41:25 +0100

    - Remove old script

--- syno_cistern/sync_rpg_to_arch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
- rsync -avz --size-only --progress --delete-after /volume1/RPG aquarion@archipelago.water.gkhs.net:/mnt/storagebox/ --exclude="#recycle" --exclude=".DS_Store" --exclude=".*" --exclude="@eaDir"
